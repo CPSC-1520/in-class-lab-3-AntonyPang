2854de51f8044107b949ac395695cc1393f6a69b
AntonyPang <apang2@nait.ca>
2024-10-02 21:12:00 -0600

    Finished lab.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 // Enter your code below.
+const javaResources = document.querySelector(".javascript-resources");
+const javaButton = document.querySelector("#show-resources");
+
+javaButton.addEventListener("click", () => {
+    javaResources.classList.remove("d-none");
+});
+
+javaResources.addEventListener("mouseover", (event) => {
+    console.log(event.target);
+    event.target.classList.add("fw-bold");
+});
+
+javaResources.addEventListener("mouseout", (event) => {
+    console.log(event.target);
+    event.target.classList.remove("fw-bold");
+});
+
+javaResources.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log(event.target);
+    event.target.classList.add("fst-italic");
+});
